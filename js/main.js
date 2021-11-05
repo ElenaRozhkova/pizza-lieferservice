5c70532de5981af1802d7de2654aa3dce0ab9de4
Elena Rozhkova <elenakuzmina88@yandex.ru>
2021-11-05 10:50:52 +0100

    feat: create login component

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.getElementById('logInForm');
+const inputLogin = document.getElementById('login');
+const inputPassword = document.getElementById('password');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+
+
+const login = (user)=> {
+    buttonOut.style.display='flex';
+    userName.style.display='flex';
+    buttonAuth.style.display='none';
+    userName.textContent=user.login;
+    modalAuth.style.display='none';
+}
+
+const logout = ()=> {
+    buttonOut.style.display='none';
+    userName.style.display='none';
+    buttonAuth.style.display='flex';
+    userName.textContent='';
+    localStorage.removeItem('user');
+}
+
+buttonAuth.addEventListener('click', ()=>{
+    modalAuth.style.display='flex';
+})
+
+buttonOut.addEventListener('click', ()=>{
+    logout();
+})
+
+closeAuth.addEventListener('click', ()=>{
+    modalAuth.style.display='none';
+})
+
+logInForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    const user = {
+        login: inputLogin.value,
+        password: inputPassword.value,
+    }
+    
+    if (!user.login) {alert("не введен логин")}
+    else {
+        login(user);
+        localStorage.setItem('user', JSON.stringify(user));
+    }   
+
+})
+
+if (localStorage.getItem('user')) {
+    login(JSON.parse(localStorage.getItem('user')));
+}
