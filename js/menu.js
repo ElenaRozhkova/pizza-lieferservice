35a826fc89e477caa46fd4023b7df7464c5033c8
Elena Rozhkova <elenakuzmina88@yandex.ru>
2021-11-07 12:48:22 +0100

    test3

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,63 @@
-const restaurant='food-band';
 
-const renderItems=(data)=>{
-    console.log(data);
+const restaurant=JSON.parse(localStorage.getItem('restaurantname'));
+
+const cardsMenu =document.querySelector('.cards-menu');
+
+const changeTitle=(restaurant)=>{
+const restaurantTitle=document.querySelector('.restaurant-title');
+restaurantTitle.textContent=restaurant.name;
+
+const rating=document.querySelector('.rating');
+rating.textContent=restaurant.stars;
+
+const price=document.querySelector('.price');
+price.textContent=`Price von ${restaurant.price} rub`;
+
+const category=document.querySelector('.category');
+category.textContent=restaurant.kitchen;
+
+
 }
 
-fetch(`./db/${restaurant}.json`)
-.then((response)=>response.json())
-.then((data)=>{
-    renderItems(data)
-})
-.catch((err)=>{console.log(err)})
+const renderItems=(data)=>{
+
+    data.forEach(({image,name,description,price,id}) => {
+        const card =document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML=`
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <div class="card-info">
+                    <div class="ingredients">${description}
+                    </div>
+                </div>
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">${price} ₽</strong>
+                </div>
+            </div>
+        `;
+        
+        cardsMenu.append(card);    
+    });
+
+}
+
+if (restaurant) {
+    changeTitle(restaurant);
+    fetch(`./db/${restaurant.products}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+        renderItems(data)
+    })
+    .catch((err)=>{console.log(err)})
+} else {
+    window.location.href='/';
+}
--- js/partners.js
+++ js/partners.js
@@ -1,12 +1,38 @@
+const cardRestaurant=document.querySelector('.cards-restaurants');
+
 const renderItems=(data)=>{
-    data.forEach(element => {
-        console.log(element.name);
-        console.log(element.time_of_delivery);
-        console.log(element.price);
-        console.log(element.image);
-        console.log(element.kitchen);
-        console.log(element.image);
-        console.log(element.products);
+    data.forEach((item) => {
+        const {name,time_of_delivery,stars,price,kitchen,image,products}=item;
+        const a = document.createElement('a');
+        a.setAttribute('href', '/restaurant.html');
+        a.classList.add('card');
+        a.classList.add('card-restaurant');
+
+        a.dataset.products=products;
+
+        a.innerHTML=`
+        <img src="${image}" alt="${image}" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title">${name}</h3>
+								<span class="card-tag tag">${time_of_delivery} мин</span>
+							</div>
+							<div class="card-info">
+								<div class="rating">
+                                ${stars}
+								</div>
+								<div class="price">От ${price} ₽</div>
+								<div class="category">${kitchen}</div>
+							</div>
+						</div>				
+        `;
+
+        a.addEventListener('click', (e)=>{
+           e.preventDefault();
+           localStorage.setItem('restaurantname', JSON.stringify(item));
+           window.location.href='/restaurant.html'
+        })
+        cardRestaurant.append(a);
     });
    
 }
